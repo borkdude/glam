35269d674711f64d597c89c95d939f9c8358816b
Michiel Borkent <michielborkent@gmail.com>
2020-09-29 12:10:06 +0200

    Bash fixes

--- resources/borkdude/glam/scripts/glam.sh
+++ resources/borkdude/glam/scripts/glam.sh
@@ -1,21 +1,27 @@
 export PATH
 export GLAM_BABASHKA
+export GLAM_GLOBAL_PATH
 
 GLAM_BABASHKA=false
-global_path=false
+GLAM_GLOBAL_PATH=""
 
+glam_global_path() {
 if [ -f "$HOME/.glam/path" ]
 then
-    PATH="$(cat "$HOME"/.glam/path 2>/dev/null):$PATH"
-    global_path=true
+    GLAM_GLOBAL_PATH=$(cat "$HOME"/.glam/path)
+    PATH="$GLAM_GLOBAL_PATH:$PATH"
 fi
+}
+
+glam_global_path
 
 glam_detect_bb() {
-if [ $global_path ] && [ -f "$HOME/.glam/repository/org.babashka/babashka/0.2.2-SNAPSHOT/bb" ]
-then
-    export GLAM_BABASHKA
-    GLAM_BABASHKA=TRUE
-fi
+    case $GLAM_GLOBAL_PATH in
+        *"$HOME/.glam/repository/org.babashka/babashka/0.2.2-SNAPSHOT"*)
+            export GLAM_BABASHKA
+            GLAM_BABASHKA=true
+            ;;
+        esac
 }
 
 glam_detect_bb
@@ -23,12 +29,13 @@ glam_detect_bb
 glam() {
     export PATH
     glam_detect_bb
+    glam_global_path
     extra_path=""
-    if [ $GLAM_BABASHKA ]
+    if [ "$GLAM_BABASHKA" = "true" ]
     then
         extra_path=$(bb -cp "$(clojure -Spath -A:glam)" -m glam.main -- "$@")
     else
-        extra_path=$(clojure -M:glam -- "$@")
+        extra_path=$(clojure -M:glam "$@")
     fi
     PATH=$extra_path:$PATH
 }
