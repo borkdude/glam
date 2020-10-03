9c2852fdcd59ed7ed1b4e0ae0455480b3731e5b0
Michiel Borkent <michielborkent@gmail.com>
2020-10-03 14:10:43 +0200

    test

--- test/glam/main_test.clj
+++ test/glam/main_test.clj
@@ -10,8 +10,10 @@
 
 (deftest install-test
   (let [output (with-out-str (main/main "install" "org.babashka/babashka@0.2.2" #_"--verbose"))
+        cache-dir (io/file "test-dir" ".cache" ".glam" "repository" "org.babashka" "babashka" "0.2.2")
         data-dir (io/file "test-dir" ".data" ".glam" "repository" "org.babashka" "babashka" "0.2.2")
         bb-executable (io/file data-dir "bb")]
+    (is (.exists cache-dir) (str cache-dir " doesn't exist"))
     (is (.exists data-dir) (str data-dir " doesn't exist"))
     (is (.exists bb-executable))
     (is (.canExecute bb-executable))
