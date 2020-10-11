118549aca7a49df6af99846e3bce7878c500b304
Michiel Borkent <michielborkent@gmail.com>
2020-10-11 20:46:40 +0200

    script

--- resources/glam/scripts/glam.sh
+++ resources/glam/scripts/glam.sh
@@ -1,19 +1,17 @@
 export PATH
-export GLAM_BABASHKA
 export GLAM_GLOBAL_PATH
 export GLAM_LOCAL_PATH
 
-GLAM_BABASHKA=false
 GLAM_GLOBAL_PATH=""
 GLAM_LOCAL_PATH=""
 GLAM_SYS_PATH=$PATH
 
 glam_global_path() {
-if [ -f "$HOME/.glam/path" ]
-then
-    GLAM_GLOBAL_PATH=$(cat "$HOME"/.glam/path)
-    PATH="$GLAM_GLOBAL_PATH:$PATH"
-fi
+    if [ -f "$HOME/.glam/path" ]
+    then
+        GLAM_GLOBAL_PATH=$(cat "$HOME"/.glam/path)
+        PATH="$GLAM_GLOBAL_PATH:$PATH"
+    fi
 }
 
 glam_local_path() {
@@ -26,17 +24,6 @@ glam_local_path() {
 
 glam_global_path
 glam_local_path
-
-# glam_detect_bb() {
-#     case $GLAM_GLOBAL_PATH in
-#         *"$HOME/.glam/repository/org.babashka/babashka/0.2.2"*)
-#             export GLAM_BABASHKA
-#             GLAM_BABASHKA=true
-#             ;;
-#         esac
-# }
-
-# glam_detect_bb
 
 glam_join_paths() {
     for I in "$@"
@@ -55,7 +42,18 @@ glam_join_paths() {
 }
 
 glam() {
-    extra_path=$(clojure -M:glam "$@")
+    # shellcheck disable=SC2153
+    if [ "$GLAM_CMD" != "" ]
+    then
+        glam_cmd="$GLAM_CMD"
+    elif [ -x "$(which glam-bin)" ]
+    then
+         glam_cmd="glam-bin"
+    else
+        >&2 echo "glam-bin not found, have you installed it and added it to the path?"
+        return 1
+    fi
+    extra_path=$(eval "$glam_cmd" "$@")
     glam_global_path
     glam_local_path
     exit_code=$?
