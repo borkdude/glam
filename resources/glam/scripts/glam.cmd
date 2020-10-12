e5804ed94c1babfc40cf0b043ab57e3b4b50cdc6
Michiel Borkent <michielborkent@gmail.com>
2020-10-12 11:02:39 +0200

    Windows WIP

--- resources/glam/scripts/glam.cmd
+++ resources/glam/scripts/glam.cmd
@@ -1,9 +1,9 @@
 @echo off
-
-set extra_path=""
-for /f "delims=" %%i in ('clojure -M:glam %*') do set extra_path="%%i"
-echo "extra: %extra_path%"
+REM echo "path before: %PATH%"
+set extra_path=
+for /f "delims=" %%i in ('clojure -M:glam %*') do set "extra_path=%%i"
 if not "%extra_path%"=="" (
-  set PATH=%extra_path%;%PATH%
+  set "PATH=%extra_path%;%PATH%"
 )
+REM echo "path after: %PATH%"
 set extra_path=
